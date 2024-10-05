046f3de5a8bd91e953a7b7a25803cdfacf1a5ff3
Tanmay Mondal <tanmoy10122000@gmail.com>
2024-10-05 23:02:56 +0530

    product-page-comp

---
+++ main.js
@@ -0,0 +1,25 @@
+const menuIcon = document.querySelector(".menu-icon");
+const backdrop = document.querySelector(".backdrop");
+const navLinks = document.querySelector(".nav-links");
+const closeIcon = document.querySelector(".close-icon");
+
+menuIcon.addEventListener("click", () => {
+  backdrop.classList.add("active");
+  navLinks.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  backdrop.classList.remove("active");
+  navLinks.classList.remove("active");
+});
+
+backdrop.addEventListener("click", () => {
+  backdrop.classList.remove("active");
+  navLinks.classList.remove("active");
+});
+
+const heart = document.getElementById('heart');
+
+  heart.addEventListener('click', function() {
+    heart.classList.toggle('red');
+  });
