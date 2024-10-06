593b3e4f50edbd822450e2ddd40f1ff30d4252f4
Tanmay Mondal <tanmoy10122000@gmail.com>
2024-10-06 13:48:00 +0530

    adjustment

--- slider.js
+++ slider.js
@@ -1,13 +1,5 @@
 const mainImages = document.querySelectorAll(".default .main-img img");
 const thumbnails = document.querySelectorAll(".default .thumb-list div");
-const lightboxMainImages = document.querySelectorAll(".lightbox .main-img img");
-const lightboxThumbnails = document.querySelectorAll(
-  ".lightbox .thumb-list div"
-);
-const lightbox = document.querySelector(".lightbox");
-const iconClose = document.querySelector(".icon-close");
-const iconPrev = document.querySelector(".icon-prev");
-const iconNext = document.querySelector(".icon-next");
 
 let currentImageIndex = 0;
 
@@ -29,33 +21,3 @@ thumbnails.forEach((thumb, index) => {
     changeImage(index, mainImages, thumbnails);
   });
 });
-
-lightboxThumbnails.forEach((thumb, index) => {
-  thumb.addEventListener("click", () => {
-    changeImage(index, lightboxMainImages, lightboxThumbnails);
-  });
-});
-
-mainImages.forEach((img, index) => {
-  img.addEventListener("click", () => {
-    lightbox.classList.add("active");
-    changeImage(index, lightboxMainImages, lightboxThumbnails);
-  });
-});
-
-iconPrev.addEventListener("click", () => {
-  if (currentImageIndex <= 0) {
-    changeImage(mainImages.length - 1, lightboxMainImages, lightboxThumbnails);
-  } else {
-    changeImage(currentImageIndex - 1, lightboxMainImages, lightboxThumbnails);
-  }
-});
-
-iconNext.addEventListener("click", () => {
-  if (currentImageIndex >= mainImages.length - 1) {
-    changeImage(0, lightboxMainImages, lightboxThumbnails);
-  } else {
-    changeImage(currentImageIndex + 1, lightboxMainImages, lightboxThumbnails);
-  }
-});
-
